793cfc4d7f6be78f018282d4a6acf5bcc3d1ed35
guokaiqiang <gkqforever@hotmail.com>
2020-10-05 17:49:09 +0800

    增加满十二

--- src/main/java/com/coc/data/constant/ClanTagConstants.java
+++ src/main/java/com/coc/data/constant/ClanTagConstants.java
@@ -11,7 +11,7 @@ public class ClanTagConstants {
      * @author guokaiqiang
      * @date 2020/8/1 10:43
      **/
-    public static final String WHITE_LIST_TAGS = "#2VJ2RCUC,#LCV289QP,#2QCJVVYC,#UPPLPYRP,#RCCVL8RU,#88CVCLVQ";
+    public static final String WHITE_LIST_TAGS = "#2VJ2RCUC,#LCV289QP,#2QCJVVYC,#UPPLPYRP,#RCCVL8RU,#88CVCLVQ,#2082J9G2Q";
 
     public static final String DEFAULT_LEAGUE_GROUP_WAR_TAG = "#0";
 }
