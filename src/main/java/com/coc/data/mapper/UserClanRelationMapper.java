8d6b34d468aca09b90452b7694b971350d458c26
guokaiqiang <gkqforever@hotmail.com>
2021-07-06 21:46:32 +0800

    增加用户跟村庄之间的关注关系表

---
+++ src/main/java/com/coc/data/mapper/UserClanRelationMapper.java
@@ -0,0 +1,8 @@
+package com.coc.data.mapper;
+
+import com.coc.data.mapper.base.BaseDao;
+import com.coc.data.model.base.UserClanRelation;
+import com.coc.data.model.base.UserClanRelationExample;
+
+public interface UserClanRelationMapper  extends BaseDao<UserClanRelation, UserClanRelationExample> {
+}
---
+++ src/main/java/com/coc/data/model/base/UserClanRelation.java
@@ -0,0 +1,75 @@
+package com.coc.data.model.base;
+
+import java.util.Date;
+
+public class UserClanRelation {
+    private Long id;
+
+    private Long userId;
+
+    private Long clanId;
+
+    private String state;
+
+    private Byte isDeleted;
+
+    private Date createTime;
+
+    private Date updateTime;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Long getClanId() {
+        return clanId;
+    }
+
+    public void setClanId(Long clanId) {
+        this.clanId = clanId;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state == null ? null : state.trim();
+    }
+
+    public Byte getIsDeleted() {
+        return isDeleted;
+    }
+
+    public void setIsDeleted(Byte isDeleted) {
+        this.isDeleted = isDeleted;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+}
